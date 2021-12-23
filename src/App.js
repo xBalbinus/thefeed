ab4454d130f7a3b17ff2cb19ee8cb8a0639bea83
Xiangan <xiangan9@gmail.com>
2021-12-22 21:08:13 -0500

    saving something that runs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
+// Setup env
+require('dotenv').config()
 const axios = require("axios"); // Requests
 const dotenv = require("dotenv"); // Env vars
 const fs = require("fs") ; // Filesystem
+const logger = require("logger");
 
-// Setup env
-require('dotenv').config()
+let BEARER = process.env.BEARER_TOKEN;
 
-let CONSUMER_KEY = process.env.CONSUMER_KEY
-let CONSUMER_SECRET = process.env.CONSUMER_SECRET
-let ACCESS_TOKEN = process.env.ACCESS_TOKEN
-let TOKEN_SECRET = process.env.TOKEN_SECRET
+let twitterID = "1347196737501462530";
 
 var config = {
-  method: 'get',
-  url: 'https://api.twitter.com/2/users/1347196737501462530/following?max_results=10',
+  method: 'GET',
+  url: 'https://api.twitter.com/2/users/' + twitterID + '/following?max_results=10',
   headers: { 
-    'Authorization': 'OAuth oauth_consumer_key="ZA8gu0ooP8ZAnd0kCYzEyo37X",oauth_token="1347196737501462530-TdywlxRi6S5XE7RiLfHBs2d0AozdNU",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1640216063",oauth_nonce="ObggRgRBmbi",oauth_version="1.0",oauth_signature="Z3mBQkZfkepICw3kWzcNa9x%2Fj2k%3D"', 
-    'Cookie': 'guest_id=v1%3A163604112176542839; guest_id_ads=v1%3A163604112176542839; guest_id_marketing=v1%3A163604112176542839; personalization_id="v1_9SdyyDTelDd3IUKGnPfKRw=="'
-  }
+   'Authorization': 'Bearer ' + BEARER + '', 
+   }
 };
 
 axios(config)
@@ -26,3 +24,5 @@ axios(config)
 .catch(function (error) {
   console.log(error);
 });
+
+
